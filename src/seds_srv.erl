msantos/seds

commit c26539cf7395abd3b88d1e134579c9e82de4d238

Specify the port in sys.config

src/seds_srv.erl
@@ -36,7 +36,7 @@
 
 -define(SERVER, ?MODULE).
 
--export([start_link/0, start_link/1]).
+-export([start_link/0]).
 -export([send/4]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
         terminate/2, code_change/3]).
@@ -60,16 +60,11 @@ send(IP, Port, #dns_rec{} = Rec, #seds{} = Query) ->
 
 -spec start_link() -> 'ignore' | {'error',_} | {'ok',pid()}.
 start_link() ->
-    start_link(?DNS_PORT).
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
--spec start_link(inet:port_number()) -> 'ignore' | {'error',_} | {'ok',pid()}.
-start_link(Port) ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
-
--spec init([inet:port_number()]) -> {'ok',#state{}}.
-init([Port]) when Port > 1024 ->
-    init(Port, []);
-init([Port]) ->
+-spec init([]) -> {'ok',#state{}}.
+init([]) ->
+    Port = application:get_env(seds, port, 53),
     {ok, FD} = procket:open(Port, [
         {protocol, udp},
         {family, inet},

include/seds.hrl
@@ -29,9 +29,7 @@
 %% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 %% POSSIBILITY OF SUCH DAMAGE.
 
--define(DNS_PORT, 53).
 -define(PROXY_TIMEOUT, 5 * 60 * 1000).
-
 -define(MAXLABEL, 63).  % 1 byte for length of label
 
 -record(seds, {

rel/sys.config.dist
@@ -4,6 +4,7 @@
 
 % Defaults to:
 % [{seds, [
+%     {port, 53},
 %     {dynamic, false},
 %     {acl, []},
 %     {allowed_ports, [22]},
@@ -13,6 +14,8 @@
 
 [
     {seds, [
+        {port, 53},
+
         %%--------------------------------------------------------------------
         %%% Static forwarding (rules set on server)
         %%--------------------------------------------------------------------

commit 327bd654912b59a13c54bb54839d2ab7584a3a1d

Use native erlang for listening on high ports
Bypass procket when listening on an unprivileged port. Broken by commit
c26539cf.

src/seds_srv.erl
@@ -60,11 +60,13 @@ send(IP, Port, #dns_rec{} = Rec, #seds{} = Query) ->
 
 -spec start_link() -> 'ignore' | {'error',_} | {'ok',pid()}.
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
-
--spec init([]) -> {'ok',#state{}}.
-init([]) ->
     Port = application:get_env(seds, port, 53),
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
+
+-spec init([inet:port_number()]) -> {'ok',#state{}}.
+init([Port]) when Port > 1024 ->
+    init(Port, []);
+init([Port]) ->
     {ok, FD} = procket:open(Port, [
         {protocol, udp},
         {family, inet},